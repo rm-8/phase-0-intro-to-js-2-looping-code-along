c3228852ba5f1bccde5e8cff8dac0a0509859c53
rm-8 <mohamedridhwan8@gmail.com>
2023-12-11 10:29:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+
+  writeCards(["Ada", "Brendan", "Ali"], "birthday");
+  
+  countDown(10);
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
